46c0e4a51067c40401b31f63acc4ab4d2a49b105
amdadislam01 <amdadislam733@gmail.com>
2025-08-19 11:28:00 +0600

    react-project-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-8xl text-center m-100 font-bold'>
+      Amdad Islam
+    </div>
+  )
+}
+
+export default App
